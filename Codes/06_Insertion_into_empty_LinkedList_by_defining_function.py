bfd9e9d4d468546676ecd40fc6a73f344ff0dd5d
Muhammad Abrar <abrar.at.hm@gmail.com>
2026-07-25 11:04:19 +0200

    Updated 06_Insertion_into_empty_LinkedList_by_defining_function

--- Codes/06_Insertion_into_empty_LinkedList_by_defining_function.py
+++ Codes/06_Insertion_into_empty_LinkedList_by_defining_function.py
@@ -3,16 +3,6 @@ class Node:
         self.data=data
         self.next=next
 
-# head=None
-# first_node=Node(10)
-# first_node.next=head
-# head=first_node
-# current = head
-# while current:
-#     print(current.data)
-#     current=current.next
-
-
 def insert_at_the_beginning(head,value):
     new_node=Node(value)
     new_node.next=head
@@ -20,6 +10,54 @@ def insert_at_the_beginning(head,value):
     return head
 head=None
 head = insert_at_the_beginning(head,20)
+Current=head
+while Current:
+    print(Current.data)
+    Current=Current.next
+print()
+
+
+
+
+# Drill 1
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data=data
+        self.next=next
+def insert_at_the_beginning2(head,value):
+    new_node1=Node(value)
+    new_node1.next=head
+    head=new_node1
+    return head
+head = None
+Current=head
+head=insert_at_the_beginning2(head,20)
+head=insert_at_the_beginning2(head,30)
+head=insert_at_the_beginning2(head,40)
+Current=head
+while Current:
+    print(Current.data)
+    Current=Current.next
+print()
+
+
+# Drill 2
+
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data=data
+        self.next=next
+def insert_at_the_beginning3(head,value):
+    new_node2=Node(value)
+    new_node2.next=head
+    head=new_node2
+    return head
+head=None
+head=insert_at_the_beginning3(head,50)
+head=insert_at_the_beginning3(head,10)
+head=insert_at_the_beginning3(head,80)
+head=insert_at_the_beginning3(head,30)
 Current=head
 while Current:
     print(Current.data)
